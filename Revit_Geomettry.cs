c7e8d74678c2d0dc19b61feb3ec14552db3772a5
Turtle Wolfe <JonPohlner@gmail.com>
2023-05-30 08:43:58 -0400

    8. Line and Curves

--- Revit_Geomettry.cs
+++ Revit_Geomettry.cs
@@ -15,14 +15,18 @@ namespace Revit_Geometry
 {
     internal class Revit_Geomettry
     {
-        public static XYZ p1 = new XYZ(0, 4, 5);
-        XYZ vector = new XYZ(0, 0, 1);
-        static XYZ unifiedVector = p1.Normalize();
-        //XYZ reversVector = -p1;
-        XYZ reversVector = p1.Negate();
-        //XYZ modifiedVector = unifiedVector * 2;
-        public static XYZ modifiedVector = unifiedVector.Multiply(3);
-        XYZ addition = p1.Add(modifiedVector);
+        public void geometryFunction()
+        {
+            XYZ p1 = new XYZ(0, 4, 5);
+            XYZ vector = new XYZ(0, 0, 1);
+            XYZ unifiedVector = p1.Normalize();
+            //XYZ reversVector = -p1;
+            XYZ reversVector = p1.Negate();
+            //XYZ modifiedVector = unifiedVector * 2;
+            XYZ modifiedVector = unifiedVector.Multiply(3);
+            XYZ addition = p1.Add(modifiedVector);
+            Line line_One = Line.CreateBound(p1, addition);
+        }
     }
 
     public class CoordinateSystem
