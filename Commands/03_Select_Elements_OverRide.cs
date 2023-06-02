7c04bb509ef1afab78b8f784cbf55ba0e3a9eea2
Turtle Wolfe <JonPohlner@gmail.com>
2023-06-02 12:52:31 -0400

    10. Creating a Basic Form From Code

---
+++ Commands/03_Select_Elements_OverRide.cs
@@ -0,0 +1,84 @@
+﻿#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Autodesk.Revit.DB.Structure;
+using System.Linq;
+#endregion
+namespace Revit_VizForms
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    internal class _03_Select_Elements_OverRide:IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            // Selection
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            Extraction EX = new Extraction();
+            EX.doc = doc;
+
+            List<string> s_values = new List<string>() { "Generic Models", "Structural Columns", "Structural Framing" };
+            string selection = Forms.single_ComboBox_Selector("Selector","Selection", s_values);
+
+
+            List<FamilyInstance> genericModels = Sel.GetAllFamilyInsancesOfCategory(doc, BuiltInCategory.OST_GenericModel);
+            List<FamilyInstance> structuralColumns = Sel.GetAllFamilyInsancesOfCategory(doc, BuiltInCategory.OST_StructuralColumns);
+            List<FamilyInstance> structuralFramings = Sel.GetAllFamilyInsancesOfCategory(doc, BuiltInCategory.OST_StructuralFraming);
+
+            List<E_element> eObjects = new List<E_element>();
+            foreach (FamilyInstance fi in genericModels)
+            {
+                E_element val = EX.Get_e_Element_from_Element(fi as Element);
+                eObjects.Add(val);
+            }
+            foreach (FamilyInstance fi in structuralColumns)
+            {
+                E_element val = EX.Get_e_Element_from_Element(fi as Element);
+                eObjects.Add(val);
+            }
+            foreach (FamilyInstance fi in structuralFramings)
+            {
+                E_element val = EX.Get_e_Element_from_Element(fi as Element);
+                eObjects.Add(val);
+            }
+            List<double> heights = new List<double>();
+            foreach (E_element e in eObjects)
+            {
+                heights.Add(Math.Round(e.bPoint.Z,1));
+            }
+            List<double> uniqueHeights = heights.Distinct().ToList();
+            List<Color> ColorSet = Graphical.create_List_of_Colors_by_Number(uniqueHeights.Count);
+            Dictionary<double, OverrideGraphicSettings> overRides_dictionary = new Dictionary<double, OverrideGraphicSettings>();
+            int i = 0;
+            foreach(double d in uniqueHeights)
+            {
+                overRides_dictionary.Add(d, Graphical.create_OverRide_ByColor_and_PatternName(doc, ColorSet[i], "<Solid fill>"));
+                i++;
+            }
+            // Analysis
+
+            // Creation
+            
+            // Transaction
+            Transaction tx = new Transaction(doc);
+            tx.Start("Transaction Trinam VizForms");
+            foreach (E_element e in eObjects)
+            {
+                doc.ActiveView.SetElementOverrides(e.element.Id, overRides_dictionary[Math.Round(e.bPoint.Z,1)]);   
+            }
+            tx.Commit();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Forms.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Revit_VizForms
+{
+    internal class Forms
+    {
+        public static string single_ComboBox_Selector(string FormText,string Title, List<string> Items)
+        {
+            string result="value";
+            Form form_One = new Form();
+            form_One.Text = FormText;
+            
+            Label label_One = new Label();
+            label_One.Text = Title;
+            label_One.Location = new Point(20, 10);
+
+            Button button_One = new Button();
+            button_One.Text = "OK";
+            button_One.DialogResult = DialogResult.OK;
+            button_One.Location = new Point(20, 100);
+
+            form_One.HelpButton = true;
+            form_One.AcceptButton = button_One;
+            form_One.StartPosition = FormStartPosition.CenterScreen;
+            
+            form_One.Controls.Add(label_One);
+            form_One.Controls.Add(button_One);
+            form_One.Size = new Size(200, 200);
+
+            form_One.ShowDialog();
+
+            return result;
+        }
+    }
+}
